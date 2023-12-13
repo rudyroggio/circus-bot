082fa59a8aba067553c444d21398bbf2d1232e42
Rudy Roggio <98646445+rudyroggio@users.noreply.github.com>
2023-12-12 23:52:35 -0500

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+DATABASE_PATH = "database.json"
+
+def load_database():
+    if os.path.exists(DATABASE_PATH):
+        with open(DATABASE_PATH, "r") as file:
+            return json.load(file)
+    return {}
+
+def save_database(database):
+    with open(DATABASE_PATH, "w") as file:
+        json.dump(database, file, indent=4)
