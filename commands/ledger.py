c0130f87bd41b6a1f6237937efd099044704d32b
Rudy Roggio <98646445+rudyroggio@users.noreply.github.com>
2023-12-14 22:38:14 -0500

    Role perm changes

--- commands/ledger.py
+++ commands/ledger.py
@@ -9,15 +9,23 @@ import os
 class LedgerCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.game_ids_file = 'processed_game_ids.txt'
+        os.makedirs('data', exist_ok=True)
+        self.game_ids_file_path = os.path.join('data', self.game_ids_file)
 
+    @commands.has_any_role("pit bosses", "eyes in the sky")
     @commands.command()
     async def ledger(self, ctx, url: str):
         try:
             game_id = self._extract_game_id(url)
-            csv_file_path = await self._download_csv(game_id)
+            if self._is_duplicate_game_id(game_id):
+                await ctx.send(f'Game ID {game_id} has already been processed.')
+                return
 
+            csv_file_path = await self._download_csv(game_id)
             if csv_file_path:
                 self._process_and_update_ledger(csv_file_path, game_id)
+                self._record_game_id(game_id)
                 await ctx.send(f'Ledger processed and database updated for game ID: {game_id}')
                 await ctx.send('The ledger has been officially closed. Thank you.')
             else:
@@ -48,6 +56,17 @@ class LedgerCommands(commands.Cog):
         self._update_database(database, grouped_df)
         save_database(database)
 
+    def _is_duplicate_game_id(self, game_id):
+        if os.path.exists(self.game_ids_file_path):
+            with open(self.game_ids_file_path, 'r') as file:
+                processed_ids = file.read().splitlines()
+            return game_id in processed_ids
+        return False
+
+    def _record_game_id(self, game_id):
+        with open(self.game_ids_file_path, 'a') as file:
+            file.write(game_id + '\n')
+            
     def _group_ledger_data(self, df):
         grouped_df = df.groupby(['player_nickname', 'player_id'])['net'].sum().reset_index()
         grouped_df['net'] = grouped_df['net'].astype(float) / 100
